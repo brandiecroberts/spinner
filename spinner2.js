4d3c626abfc33a51fb2d2c6685ac1fb7acd6e97a
Brandie Roberts <brandiecroberts@gmail.com>
2023-01-17 16:16:58 -0800

    Refactored spinner1.js

---
+++ spinner2.js
@@ -0,0 +1,37 @@
+let special = ['/', '!', '$', '@', '\\'];
+let delay = 200;
+
+for (let i = 0; i < special.length; i++) {
+  setTimeout(() => {
+    process.stdout.write("\r" + special[i]);
+  }, delay += 100);
+}
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r;   ');
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r.   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r<   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r>   ');
+// }, 1500);
